88b50fe60c66df81e4796cad4a2959e047df0d6c
JunsuChoi <jsuya.choi@samsung.com>
2023-09-18 16:16:40 +0900

    [Temporary] C# multi app example

---
+++ temporary_example/multi_service_app/tizen/App.cs
@@ -0,0 +1,20 @@
+﻿using Tizen.Flutter.Embedding;
+
+namespace RunnerService
+{
+    public class App : FlutterServiceApplication
+    {
+        protected override void OnCreate()
+        {
+            DartEntrypoint = "serviceMain";
+            base.OnCreate();
+            GeneratedPluginRegistrant.RegisterPlugins(this);
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.Run(args);
+        }
+    }
+}
