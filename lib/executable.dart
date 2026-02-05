b97309976f265b1dfe9eaf8340ebf3c35c722518
JunsuChoi <jsuya.choi@samsung.com>
2026-02-05 13:59:50 +0900

    Respect --machine flag for JSON-safe logging
    Use args.contains('--machine') when configuring the Logger so machine mode applies consistently (matches upstream flutter_tools executable).
    
    Refs: flutter/flutter@19074d12f7eaf6a8180cd4036a430c1d76de904e
    https://github.com/flutter/flutter/blob/19074d12f7eaf6a8180cd4036a430c1d76de904e/packages/flutter_tools/lib/executable.dart

--- lib/executable.dart
+++ lib/executable.dart
@@ -83,7 +83,8 @@ Future<void> main(List<String> args) async {
   final bool verboseHelp = help && verbose;
   final bool daemon = args.contains('daemon');
   final bool widgetPreviews = args.contains(WidgetPreviewCommand.kWidgetPreview);
-  final bool runMachine = args.contains('--machine') && args.contains('run');
+  // Match upstream behavior: --machine controls JSON-safe logging.
+  final bool runMachine = args.contains('--machine');
 
   Cache.flutterRoot = join(rootPath, 'flutter');
 
