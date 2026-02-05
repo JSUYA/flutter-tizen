b73211f25612f6792c101be13ff6f02ba45e7592
JunsuChoi <jsuya.choi@samsung.com>
2026-02-05 14:37:58 +0900

    Support --performance-measurement-file in build module
    Expose the upstream build performance output option for build module.
    
    Refs: flutter/flutter@19074d12f7eaf6a8180cd4036a430c1d76de904e
    https://github.com/flutter/flutter/blob/19074d12f7eaf6a8180cd4036a430c1d76de904e/packages/flutter_tools/lib/src/commands/build_apk.dart

--- lib/commands/build.dart
+++ lib/commands/build.dart
@@ -97,6 +97,7 @@ class BuildModuleCommand extends BuildSubCommand with DartPluginRegistry, TizenR
     addEnableExperimentation(hide: !verboseHelp);
     addSplitDebugInfoOption();
     addTreeShakeIconsFlag();
+    addBuildPerformanceFile(hide: !verboseHelp);
     usesDartDefineOption();
     usesExtraDartFlagOptions(verboseHelp: verboseHelp);
     usesPubOption();
